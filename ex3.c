c9b2faa33d0a0d4bf07ed5c7c48de16fca74e138
nhh20204963 <81414202+nhh20204963@users.noreply.github.com>
2021-04-05 11:26:47 +0700

    Add files via upload

---
+++ ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main ()
+ {
+ 	char character = 'A';
+ 	char string[] = "This is a sting";
+ 	const char *stringPtr = "This is also a string";
+ 	
+ 	printf( "%c\n", character );
+ 	printf( "%s\n", "This is a string" );
+ 	printf( "%s\n", string );
+ 	printf( "%s\n", stringPtr);
+ 	
+ 	return 0;
+ }
+
